86056d05263350f4a1ae09739eddb763e205d42c
Kevon Kibochi <devokrann@gmail.com>
2024-08-25 20:21:23 +0300

    Add providers

--- providers/context/Products.tsx
+++ providers/context/Products.tsx
@@ -4,44 +4,22 @@ import React, { SetStateAction, useCallback, useEffect, useState } from "react";
 
 import ContextProducts from "@/contexts/Products";
 
-import { typeProduct } from "@/types/product";
+import { typeOrder } from "@/types/order";
+import { typeCart } from "@/types/cart";
+
+import dataWishlist from "@/data/wishlist";
+import dataCart from "@/data/cart";
+import dataOrders from "@/data/orders";
+import { typeWishlist } from "@/types/wishlist";
 
 export default function Products({ children }: { children: React.ReactNode }) {
-	const [wishlist, setWishlist] = useState<typeProduct[]>(() => {
-		try {
-			const storedWishlist = typeof window !== "undefined" ? window.localStorage.getItem("wishlist") : null;
-			return storedWishlist ? JSON.parse(storedWishlist) : [];
-		} catch (error) {
-			console.error("x-> Failed to parse wishlist from localStorage:", (error as Error).message);
-			return [];
-		}
-	});
-	const [cart, setCart] = useState<typeProduct[]>(() => {
-		try {
-			const storedCart = typeof window !== "undefined" ? window.localStorage.getItem("cart") : null;
-			return storedCart ? JSON.parse(storedCart) : [];
-		} catch (error) {
-			console.error("x-> Failed to parse cart from localStorage:", (error as Error).message);
-			return [];
-		}
-	});
-
-	useEffect(() => {
-		if (typeof window !== "undefined") {
-			window.localStorage.setItem("wishlist", JSON.stringify(wishlist));
-
-			// window.localStorage.clear();
-		}
-	}, [wishlist]);
-	useEffect(() => {
-		if (typeof window !== "undefined") {
-			window.localStorage.setItem("cart", JSON.stringify(cart));
-
-			// window.localStorage.clear();
-		}
-	}, [cart]);
+	const [wishlist, setWishlist] = useState<typeWishlist[]>(dataWishlist);
+	const [cart, setCart] = useState<typeCart[]>(dataCart);
+	const [orders, setOrders] = useState<typeOrder[]>(dataOrders);
 
 	return (
-		<ContextProducts.Provider value={{ wishlist, setWishlist, cart, setCart }}>{children}</ContextProducts.Provider>
+		<ContextProducts.Provider value={{ wishlist, setWishlist, cart, setCart, orders, setOrders }}>
+			{children}
+		</ContextProducts.Provider>
 	);
 }
---
+++ providers/products/Cart.tsx
@@ -0,0 +1,157 @@
+"use client";
+
+import React, { useContext, useEffect, useState } from "react";
+
+import ContextProducts from "@/contexts/Products";
+import { typeProduct, typeVariant, typeProductVariant } from "@/types/product";
+import { notifications } from "@mantine/notifications";
+import { IconShoppingCartMinus, IconShoppingCartPlus, IconShoppingCartX } from "@tabler/icons-react";
+import { Box, Text } from "@mantine/core";
+import array from "@/utilities/array";
+import compoundId from "@/handlers/parsers/string/compoundId";
+
+export default function Cart({
+	operation,
+	children,
+}: {
+	operation: {
+		type: "add" | "remove" | "decrease" | "increase" | "clear";
+		items?: typeProductVariant[];
+		unmount?: boolean;
+		quantity?: number;
+	};
+	children: React.ReactNode;
+}) {
+	const productsContext = useContext(ContextProducts);
+
+	if (!productsContext) {
+		throw new Error("ChildComponent must be used within a MyContext.Provider");
+	}
+
+	const { cart, setCart } = productsContext;
+
+	const [mounted, setMounted] = useState(true);
+
+	const addToCart = (set: typeProductVariant[]) => {
+		const itemsToIgnore = set.filter(item => array.isPresent(compoundId.getCompoundId(item), cart));
+
+		if (itemsToIgnore.length != set.length) {
+			// Filter out all items already included
+			const compoundIdsToIgnore = itemsToIgnore.map(item => compoundId.getCompoundId(item));
+			setCart(
+				cart.concat(
+					set
+						.map(s =>
+							!compoundIdsToIgnore.includes(compoundId.getCompoundId(s))
+								? {
+										...s,
+										id: compoundId.getCompoundId(s),
+										quantity: set.length > 1 ? 1 : operation.quantity ? operation.quantity : 1,
+								  }
+								: undefined
+						)
+						.filter(p => p != undefined)
+				)
+			);
+
+			notifications.show({
+				id: `cart-add-${set.map(item => compoundId.getCompoundId(item)).join("-")}`,
+				icon: <IconShoppingCartPlus size={16} stroke={1.5} />,
+				title: `Added to Cart`,
+				message: (
+					<Text inherit>
+						{set.length > 1 ? (
+							`${set.length} items`
+						) : (
+							<Text component="span" inherit fw={500} c={"sl.4"}>
+								{set[0].product.title}
+							</Text>
+						)}{" "}
+						added to your your cart.
+					</Text>
+				),
+				variant: "success",
+			});
+		} else {
+			notifications.show({
+				id: `cart-added`,
+				icon: <IconShoppingCartPlus size={16} stroke={1.5} />,
+				title: `Already Added`,
+				message: `${set.length > 1 ? "All selected items" : set[0].product.title} already added to your cart.`,
+				variant: "info",
+			});
+		}
+	};
+
+	const removeFromCart = (set: typeProductVariant[]) => {
+		const itemsToRemove = set.filter(item => array.isPresent(compoundId.getCompoundId(item), cart));
+
+		if (itemsToRemove.length > 0) {
+			// Update the cart by filtering out all items in one go
+			const compoundIdsToRemove = itemsToRemove.map(item => compoundId.getCompoundId(item));
+			setCart(cart.filter(p => !compoundIdsToRemove.includes(p.id)));
+
+			notifications.show({
+				id: `cart-remove-${itemsToRemove.map(item => compoundId.getCompoundId(item)).join("-")}`,
+				icon: <IconShoppingCartMinus size={16} stroke={1.5} />,
+				title: `Removed From Cart`,
+				message: (
+					<Text inherit>
+						{itemsToRemove.length > 1 ? (
+							`${itemsToRemove.length} items`
+						) : (
+							<Text component="span" inherit fw={500} c={"sl.4"}>
+								{itemsToRemove[0].product.title}
+							</Text>
+						)}{" "}
+						removed from your cart.
+					</Text>
+				),
+				variant: "failed",
+			});
+		} else {
+			notifications.show({
+				id: `cart-removed`,
+				icon: <IconShoppingCartMinus size={16} stroke={1.5} />,
+				title: `Already Removed`,
+				message: `All selected items already removed from your cart.`,
+				variant: "info",
+			});
+		}
+	};
+
+	const updateQuantity = {
+		decrease(id: string) {
+			setCart(cart.map(p => (p.id == id ? { ...p, quantity: p.quantity > 1 ? p.quantity - 1 : p.quantity } : p)));
+		},
+
+		increase(id: string) {
+			setCart(
+				cart.map(p => (p.id == id ? { ...p, quantity: p.quantity < 99 ? p.quantity + 1 : p.quantity } : p))
+			);
+		},
+	};
+
+	const getOperation = () => {
+		if (!operation.items) {
+			operation.type == "clear" && setCart([]);
+		} else {
+			operation.type == "add" && addToCart(operation.items);
+			operation.type == "remove" && removeFromCart(operation.items);
+			operation.type == "decrease" && updateQuantity.decrease(compoundId.getCompoundId(operation.items[0]));
+			operation.type == "increase" && updateQuantity.increase(compoundId.getCompoundId(operation.items[0]));
+		}
+
+		operation.unmount && setMounted(false);
+	};
+
+	useEffect(() => {
+		setMounted(cart.length > 0);
+	}, [cart]);
+
+	return (
+		<Box onClick={getOperation} display={operation.unmount ? (mounted ? undefined : "none") : undefined}>
+			{children}
+		</Box>
+	);
+}
