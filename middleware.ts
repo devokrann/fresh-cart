b99487b9b80f5ed158c302c7a1a6c5590c6a88f0
Kevon Kibochi <devokrann@gmail.com>
2024-09-04 21:03:35 +0300

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,10 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+// import { NextRequest } from "next/server";
 
 export const { auth: middleware } = NextAuth(authConfig);
+
+// const { auth } = NextAuth(authConfig);
+// export default auth(async function middleware(req: NextRequest) {
+// 	// Your custom middleware logic goes here
+// });
